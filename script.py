44ad12c37f04e10afa3373def60f0be410c91ac7
temuchinn <artem.moiseev.nn@gmail.com>
2022-08-29 16:18:51 +0300

    Added company name extraction and insights dumping

--- script.py
+++ script.py
@@ -1,9 +1,16 @@
 import re
 import argparse
+import json
+
 import pandas as pd
 import numpy as np
+
 import nltk
 import pymorphy2
+
+from yargy import Parser, rule
+from yargy.predicates import gram,  dictionary
+from yargy.interpretation import fact
 
 
 REGULAR_EXPRESSIONS_FOR_PARSING = {
@@ -14,6 +21,69 @@ REGULAR_EXPRESSIONS_FOR_PARSING = {
 }
 
 
+Name = fact(
+    'Name',
+    ['first', 'last']
+)
+
+
+NAME_INTR = rule(
+    dictionary({
+        'зовут',
+        'меня',
+        'я',
+        'это',
+    }),
+    gram('Name')\
+    .interpretation(
+        Name.first.inflected()
+    )
+).interpretation(Name)
+
+
+COMPANY = rule(
+    dictionary({
+        'компания',
+        'компанию',
+        'организация',
+        'организацию'
+    }),
+    gram('NOUN'),
+    gram('NOUN').optional().repeatable(),
+)
+
+
+class YargyParser:
+    """Class, containing methods for using yargy parser to extract text entities."""
+    
+    @staticmethod
+    def extract_introduce_names(text: str, lower: bool = True):
+        parser = Parser(NAME_INTR)
+        output = []
+        if lower:
+            _text = text.lower()
+            
+        for match in parser.findall(_text):
+            name = match.fact.first
+            output.append(name)
+        
+        return list(set(output))
+    
+    @staticmethod
+    def extract_company(text: str, lower: bool = True):
+        parser = Parser(COMPANY)
+        output = []
+        if lower:
+            _text = text.lower()
+        
+        for match in parser.findall(_text):
+            company_name = ' '.join([_.value for _ in match.tokens][1:])
+            if len(company_name) > 2:
+                output.append(company_name)
+        
+        return list(set(output))
+
+        
 class NerExtractor:
     """Class containing methods for NER.
     Methods:
@@ -21,19 +91,18 @@ class NerExtractor:
         
     """
     NAME_TAG = 'Name'
-
-    def __init__(self) -> None:
-        self.morph_analyzer = pymorphy2.MorphAnalyzer()
+    morph_analyzer = pymorphy2.MorphAnalyzer()
     
-    def extract_names(self, text: str, prob_thresh: float = 0.4) -> list[str]:
+    @classmethod
+    def extract_names(cls, text: str, prob_thresh: float = 0.4) -> list[str]:
         names = []
 
         for word in nltk.word_tokenize(text):
-            for p in self.morph_analyzer.parse(word):
-                if self.NAME_TAG in p.tag and p.score >= prob_thresh:
+            for p in cls.morph_analyzer.parse(word):
+                if cls.NAME_TAG in p.tag and p.score >= prob_thresh:
                     names.append(p.normal_form)
                     break
-        return names
+        return list(set(names))
 
 
 def build_arg_parser():
@@ -64,7 +133,7 @@ def make_output_path(path: str) -> str:
 
 
 def find_regexp(reg_exp: str, lower: bool=True):
-    """Returns function, checking regexp
+    """Returns function, checking regexp.
     Args:
         reg_exp (str): regular expression, which will be checked
         lower (bool): flag, defining whether to lower the input string
@@ -83,7 +152,7 @@ def find_regexp(reg_exp: str, lower: bool=True):
 
 
 def get_insights(df: pd.DataFrame, dlg_id: int) -> dict[str, any]:
-    """Returns dict with insights from dataframe
+    """Returns dict with insights from dataframe.
     Args:
         df (DataFrame): dataframe, containing supportive columns
         dlg_id (int): id of a dialogue, which insights will be received
@@ -93,12 +162,16 @@ def get_insights(df: pd.DataFrame, dlg_id: int) -> dict[str, any]:
     """
     insights = {}
     name_extractor = NerExtractor()
-    
+    yp = YargyParser()
+
     dlg_df = df[df['dlg_id'] == dlg_id]
-    insights['manager_greeting'] = np.any(dlg_df[(dlg_df['role'] == 'manager')].greeting)
-    insights['manager_farewell'] = np.any(dlg_df[(dlg_df['role'] == 'manager')].farewell)
-    insights['manager_introduced'] = np.any(dlg_df[(dlg_df['role'] == 'manager')].introduce)
+    insights['manager_greeting'] = bool(np.any(dlg_df[(dlg_df['role'] == 'manager')].greeting))
+    insights['manager_farewell'] = bool(np.any(dlg_df[(dlg_df['role'] == 'manager')].farewell))
+    insights['manager_introduced'] = bool(np.any(dlg_df[(dlg_df['role'] == 'manager')].introduce))
     insights['all_names'] = name_extractor.extract_names(' '.join(dlg_df.text.to_list()))
+    insights['manager_name'] = yp.extract_introduce_names(' '.join(dlg_df[(dlg_df['role'] == 'manager')].text.to_list()))
+    insights['client_name'] = yp.extract_introduce_names(' '.join(dlg_df[(dlg_df['role'] == 'client')].text.to_list()))
+    insights['company'] = yp.extract_company(' '.join(dlg_df.text.to_list()))
 
     return insights
 
@@ -121,19 +194,25 @@ if __name__ == '__main__':
     
     dialogues_insights = dict()
     for dlg_id in df.dlg_id.unique():
-        dialogues_insights[dlg_id] = get_insights(df, dlg_id)
+        dialogues_insights[int(dlg_id)] = get_insights(df, dlg_id)
 
     parsed_file_path = make_output_path(args.path)
     df.to_csv(parsed_file_path)
-
+    
+    with open('insights.json', mode='w', encoding='utf-8') as fp:
+        json.dump(dialogues_insights ,fp)
+    
     for dlg_id, insights in dialogues_insights.items():
         print("Dialogue id:", dlg_id)
         print("Manager_greeting:", insights['manager_greeting'])
         print("Manager_farewell:", insights['manager_farewell'])
         print("greeting + farewell:"    , insights['manager_greeting'] and insights['manager_farewell'])
-        print("manager indtroduced:", insights['manager_introduced'])
-        print("names in dialogue:", insights['all_names'])
-        
+        print("Manager indtroduced:", insights['manager_introduced'])
+        print("Names in dialogue:", insights['all_names'])
+        print("Manager introduced name:", insights['manager_name'])
+        print("Client introduced name:", insights['client_name'])
+        print("Company:", insights['company'] )
         print()
     
+
     print(f'\nSaved parsed file to "{parsed_file_path}"!\n')
